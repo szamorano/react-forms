9c02f26dfc8e0775e02e504e9b04465712cb7714
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-23 18:32:47 -0400

    Getting user input via state.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("submitted!");
+    console.log(enteredEmail);
+  }
+
+  function handleEmailChange(event) {
+    setEnteredEmail(event.target.value);
   }
 
   return (
@@ -11,7 +20,13 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+            value={enteredEmail}
+          />
         </div>
 
         <div className="control no-margin">
