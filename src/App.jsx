bcc758bc1c88d63f1a268525de2bc1aff68ca278
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-23 19:45:46 -0400

    Getting values via FormData

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
+import Header from "./components/Header.jsx";
+import Signup from "./components/SignUp.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
